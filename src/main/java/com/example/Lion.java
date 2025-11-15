4c9e82d6c30ae943a71401c798cdddfb430e8806
Ваше Имя <@email.com>
2025-11-15 19:26:56 +0300

    изоляция Lion от Feline  через инъекцию зависимостей

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Lion {
 
     private boolean hasMane;
-    private Predator predator;
+    private Feline feline;
 
-    public Lion(String sex, Predator predator) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -16,11 +16,11 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
 
-        this.predator = predator;
+        this.feline = feline;
     }
 
     public int getKittens() {
-        return predator.getKittens();
+        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -28,6 +28,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return predator.eatMeat();
+        return feline.eatMeat();
     }
 }
